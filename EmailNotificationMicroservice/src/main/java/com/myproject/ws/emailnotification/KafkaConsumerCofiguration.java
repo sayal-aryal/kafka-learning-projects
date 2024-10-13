06ce3e25f6daef8d4022cdcf86722d358919e548
sayal-aryal <sayalaryal2@gmail.com>
2024-10-13 11:15:42 -0500

    consumer bean configuraion and Kafka Listener Container factory

---
+++ EmailNotificationMicroservice/src/main/java/com/myproject/ws/emailnotification/KafkaConsumerCofiguration.java
@@ -0,0 +1,49 @@
+package com.myproject.ws.emailnotification;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+public class KafkaConsumerCofiguration {
+
+    @Autowired
+    Environment environment;
+
+    //ConsumerFactory is a Interface provided by a spring kafka used to create a instances of a kafka consumer
+//DefaultKafkaConsumerFactory is a class  which implement ConsumerFactory interface and it provides default implementation of the create consumer method .
+// It takes map of configuration property as an input and returns an instance of the kafka consumer.
+//Instead of providing the consumer configuration properties in the application.properties we provide here in this hashmap as key value pair.
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    //This method  use this configuraiton to create kafkalistenercontainer.And kafkalistenercontainer is reposnible to receiving the message from kafka topic and invoking the handler method or listener method in java code
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+
+}
